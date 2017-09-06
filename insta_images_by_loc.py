4b08ca0029671def7133ab7ad7b9ccec000bda21
jiepu2 <pujiezhi@gmail.com>
2017-09-07 09:53:23 +1000

    Add Instagram crawler

---
+++ insta_images_by_loc.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from geoloc import coordinates
+
+client_id="fcea843200a7447b967cc616e9efa79d"                              #client ID
+client_secret="eba26794ddee490687e35cdb172357d5"                          #client Secret
+redirect_uri="http://localhost"                                           #redirect uri. I've set mine to localhost. This can be changed
+grant_type="authorization_code"                                           #default option provided by Instagram
+
+def get_access_token():
+    access_token="715368389.e029fea.2c76ea92adff40db8258679d1064d7ab"
+    return access_token
+
+def media_search(lat,lng,access_token):
+    url="https://api.instagram.com/v1/media/search?lat="+lat+"&lng="+lng+"&distance=5000"+"&access_token="+access_token
+    r=requests.get(url)
+    resp=json.loads(r.text)
+    for i in range(19):                                                         #just getting 10 images. can be changed
+        pic_url=resp['data'][i]['images']['standard_resolution']['url']         #getting the image url
+        p=requests.get(pic_url)
+        f_name=str(location)+ ' pic '+str(i)+'.jpg'
+        with open(f_name,'wb') as f:
+            f.write(p.content)
+            f.close()
+        print("Image No." + i)
+
+location=input("Please enter the city name.\n")
+c=coordinates(location)
+lat=str(c[0])
+lng=str(c[1])
+
+access_token=get_access_token()
+media_search(lat, lng, access_token)
