31d42abe0e80e82cda55b0bf30e3372f76f885c5
jiepu2 <pujiezhi@gmail.com>
2017-09-07 09:58:53 +1000

    Output fixed.

--- insta_images_by_loc.py
+++ insta_images_by_loc.py
@@ -4,7 +4,7 @@ from geoloc import coordinates
 
 client_id="fcea843200a7447b967cc616e9efa79d"                              #client ID
 client_secret="eba26794ddee490687e35cdb172357d5"                          #client Secret
-redirect_uri="http://localhost"                                           #redirect uri. I've set mine to localhost. This can be changed
+redirect_uri="http://localhost"                                           #redirect uri
 grant_type="authorization_code"                                           #default option provided by Instagram
 
 def get_access_token():
@@ -15,14 +15,15 @@ def media_search(lat,lng,access_token):
     url="https://api.instagram.com/v1/media/search?lat="+lat+"&lng="+lng+"&distance=5000"+"&access_token="+access_token
     r=requests.get(url)
     resp=json.loads(r.text)
-    for i in range(19):                                                         #just getting 10 images. can be changed
+    num = 20
+    for i in range(num):
         pic_url=resp['data'][i]['images']['standard_resolution']['url']         #getting the image url
         p=requests.get(pic_url)
         f_name=str(location)+ ' pic '+str(i)+'.jpg'
         with open(f_name,'wb') as f:
             f.write(p.content)
             f.close()
-        print("Image No." + i)
+        print("Image No." + str(i) + " Downloaded")
 
 location=input("Please enter the city name.\n")
 c=coordinates(location)
