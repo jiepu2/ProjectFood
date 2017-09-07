a4b1380a1d7ff3af3bba9c423137900a4fd566a2
jiepu2 <pujiezhi@gmail.com>
2017-09-07 11:18:09 +1000

    Limit of 20 images each time solved.

--- insta_images_by_loc.py
+++ insta_images_by_loc.py
@@ -8,22 +8,25 @@ redirect_uri="http://localhost"                                           #redir
 grant_type="authorization_code"                                           #default option provided by Instagram
 
 def get_access_token():
-    access_token="715368389.e029fea.2c76ea92adff40db8258679d1064d7ab"
+    access_token="715368389.0912694.a9b928f66e5e4ed5997b9012e4ddc602"
     return access_token
 
 def media_search(lat,lng,access_token):
-    url="https://api.instagram.com/v1/media/search?lat="+lat+"&lng="+lng+"&distance=5000"+"&access_token="+access_token
-    r=requests.get(url)
-    resp=json.loads(r.text)
-    num = 20
-    for i in range(num):
-        pic_url=resp['data'][i]['images']['standard_resolution']['url']         #getting the image url
-        p=requests.get(pic_url)
-        f_name=str(location)+ ' pic '+str(i)+'.jpg'
-        with open(f_name,'wb') as f:
-            f.write(p.content)
-            f.close()
-        print("Image No." + str(i) + " Downloaded")
+    max_timestamp = 1504746309
+    for j in range(20):
+        url="https://api.instagram.com/v1/media/search?lat="+lat+"&lng="+lng+"&distance=5000"+"&access_token="+access_token + "&max_timestamp="+str(max_timestamp)
+        r=requests.get(url)
+        resp=json.loads(r.text)
+        num = 20
+        for i in range(num):
+            pic_url=resp['data'][i]['images']['standard_resolution']['url']         #getting the image url
+            p=requests.get(pic_url)
+            f_name=str(location)+ ' pic ' + str(i+j*20) + '.jpg'
+            with open(f_name,'wb') as f:
+                f.write(p.content)
+                f.close()
+            print("Image No." + str(i+j*20) + " Downloaded")
+            max_timestamp = int(resp['data'][i]['created_time'])
 
 location=input("Please enter the city name.\n")
 c=coordinates(location)
